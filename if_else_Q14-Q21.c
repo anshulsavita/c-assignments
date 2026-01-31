e1e6b6ea0401c3bcaa36c3484d0bb4f545f81fae
Anshul savita <anshulsavita370@gmail.com>
2026-01-31 20:31:00 +0530

    edit incomplete Question

--- if_else_Q14-Q21.c
+++ if_else_Q14-Q21.c
@@ -1,146 +1,158 @@
 #include<stdio.h>
 void main(){
     // *****Question 14******
-    int a,b,c;
-    printf("Enter angle 1:");
-    scanf("%d",&a);
-    printf("Enter angle 2:");
-    scanf("%d",&b);
-    printf("Enter angle 3:");
-    scanf("%d",&c);
+    // int a,b,c;
+    // printf("Enter angle 1:");
+    // scanf("%d",&a);
+    // printf("Enter angle 2:");
+    // scanf("%d",&b);
+    // printf("Enter angle 3:");
+    // scanf("%d",&c);
 
-    if(a+b+c==180){
-        printf("It is a valid triangle..");
-    }
-    else{
-        printf("It is not a valid..");
-    }
+    // if(a+b+c==180){
+    //     printf("It is a valid triangle..");
+    // }
+    // else{
+    //     printf("It is not a valid..");
+    // }
 
-    // ******Question 15*****
-    int a,b,c;
-    printf("Enter side 1:");
-    scanf("%d",&a);
-    printf("Enter side 2:");
-    scanf("%d",&b);
-    printf("Enter side 3:");
-    scanf("%d",&c);
+    // // ******Question 15*****
+    // int a,b,c;
+    // printf("Enter side 1:");
+    // scanf("%d",&a);
+    // printf("Enter side 2:");
+    // scanf("%d",&b);
+    // printf("Enter side 3:");
+    // scanf("%d",&c);
 
-    if(a+b>c && a+c>b && b+c>a){
-        printf("It is a valid triangle..");
-    }
-    else{
-        printf("It is not a valid..");
-    }
+    // if(a+b>c && a+c>b && b+c>a){
+    //     printf("It is a valid triangle..");
+    // }
+    // else{
+    //     printf("It is not a valid..");
+    // }
 
     // *****Question 16****
-    int a,b,c;
-    printf("Enter side 1:");
-    scanf("%d",&a);
-    printf("Enter side 2:");
-    scanf("%d",&b);
-    printf("Enter side 3:");
-    scanf("%d",&c);
+    // int a,b,c;
+    // printf("Enter side 1:");
+    // scanf("%d",&a);
+    // printf("Enter side 2:");
+    // scanf("%d",&b);
+    // printf("Enter side 3:");
+    // scanf("%d",&c);
 
-    if(a==b==c){
-        printf("It is an Equilateral Triangle..");
-    }
-    else if(a==b || b==c || a==c){
-        printf("It is an Isosceles Triangle..");
-    }
-    else if(a!=b!=c){
-        printf("It ia an Scalene Triangle..");
-    }
+    // if(a==b==c){
+    //     printf("It is an Equilateral Triangle..");
+    // }
+    // else if(a==b || b==c || a==c){
+    //     printf("It is an Isosceles Triangle..");
+    // }
+    // else if(a!=b!=c){
+    //     printf("It ia an Scalene Triangle..");
+    // }
 
     // ****Question 17*****
-    // incomplite...
+    // float a,b,c,d,e;
+    // printf("Enter value of a: ");
+    // scanf("%f",&a);
+
+    // printf("Enter value of b: ");
+    // scanf("%f",&b);
+
+    // printf("Enter value of c: ");
+    // scanf("%f",&c);
+
+    // d=(-b+sqrt(b*b-4*a*c))/(2*a);
+    // e=(-b-sqrt(b*b-4*a*c))/(2*a);
+    // printf("Root1 = %.2f\nRoot2 = %.2f",d,e);
 
     // ****Question 18*****
-    float a,b,c,d,p,l;
-    printf("Enter Cost price in Rs: ");
-    scanf("%f",&a);
-    printf("Enter Selling price in Rs: ");
-    scanf("%f",&b);
+    // float a,b,c,d,p,l;
+    // printf("Enter Cost price in Rs: ");
+    // scanf("%f",&a);
+    // printf("Enter Selling price in Rs: ");
+    // scanf("%f",&b);
 
-    if(a>b){
-        c=a-b;
-        l=(c/a)*100;
-        printf("You are in loss..\nLoss= %.2f Rs\nLoss percentage: %.2f%%",c,l);
-    }
-    else if(a<b){
-        d=b-a;
-        p=(d/a)*100;
-        printf("You are in profit..\nProfit= %.2f Rs\nProfit percentage: %.2f%%",d,p);
-    }
+    // if(a>b){
+    //     c=a-b;
+    //     l=(c/a)*100;
+    //     printsqrtf("You are in loss..\nLoss= %.2f Rs\nLoss percentage: %.2f%%",c,l);
+    // }
+    // else if(a<b){
+    //     d=b-a;
+    //     p=(d/a)*100;
+    //     printf("You are in profit..\nProfit= %.2f Rs\nProfit percentage: %.2f%%",d,p);
+    // }
 
-    // *****Question 19******
-    float p,h,b,m,c,t,per;
-    printf("Enter Physics marks: ");
-    scanf("%f",&p);
-    printf("Enter Chemistry marks: ");
-    scanf("%f",&h);
-    printf("Enter Biology marks: ");
-    scanf("%f",&b);
-    printf("Enter Mathematics marks: ");
-    scanf("%f",&m);
-    printf("Enter Computer marks: ");
-    scanf("%f",&c);
+    // // *****Question 19******
+    // float p,h,b,m,c,t,per;
+    // printf("Enter Physics marks: ");
+    // scanf("%f",&p);
+    // printf("Enter Chemistry marks: ");
+    // scanf("%f",&h);
+    // printf("Enter Biology marks: ");
+    // scanf("%f",&b);
+    // printf("Enter Mathematics marks: ");
+    // scanf("%f",&m);
+    // printf("Enter Computer marks: ");
+    // scanf("%f",&c);
 
-    t=p+c+b+m+h;
-    per=t/5;
+    // t=p+c+b+m+h;
+    // per=t/5;
 
-    if(per>=90 && per<=100){
-        printf("percentage: %.2f%%\nGrade A",per);
-    }
-    if(per>=80){
-        printf("percentage: %.2f%%\nGrade B",per);
-    }
-    if(per>=70){
-        printf("percentage: %.2f%%\nGrade C",per);
-    }
-    if(per>=60){
-        printf("percentage: %.2f%%\nGrade D",per);
-    }
-    if(per>=40){
-        printf("percentage: %.2f%%\nGrade E",per);
-    }
-    else{
-        printf("percentage: %.2f%%\nGrade F",per);
-    }
+    // if(per>=90 && per<=100){
+    //     printf("percentage: %.2f%%\nGrade A",per);
+    // }
+    // if(per>=80){
+    //     printf("percentage: %.2f%%\nGrade B",per);
+    // }
+    // if(per>=70){
+    //     printf("percentage: %.2f%%\nGrade C",per);
+    // }
+    // if(per>=60){
+    //     printf("percentage: %.2f%%\nGrade D",per);
+    // }
+    // if(per>=40){
+    //     printf("percentage: %.2f%%\nGrade E",per);
+    // }
+    // else{
+    //     printf("percentage: %.2f%%\nGrade F",per);
+    // }
 
-    // *****Question 20******
-    float i,da,hra,gs;
-    printf("Enter the basic salary: ");
-    scanf("%f",&i);
+    // // *****Question 20******
+    // float i,da,hra,gs;
+    // printf("Enter the basic salary: ");
+    // scanf("%f",&i);
 
-    if(i>=100000){
-        da=i*80/100;
-        hra=i*20/100;
-        gs=i+da+hra;
-        printf("da: %.2f\n",da);
-        printf("hra: %.2f\n",hra);
-        printf("gross salary: %.2f\n",gs);
-    }
-    else if(i>=20000 && i<100000){
-        da=i*90/100;
-        hra=i*25/100;
-        gs=i+da+hra;
-        printf("da: %.2f\n",da);
-        printf("hra: %.2f\n",hra);
-        printf("gross salary: %.2f\n",gs);
-    }
-    else if(i>=30000 && i<20000){
-        da=i*95/100;
-        hra=i*30/100;
-        gs=i+da+hra;
-        printf("da: %.2f\n",da);
-        printf("hra: %.2f\n",hra);
-        printf("gross salary: %.2f\n",gs);
-    }
-    else{
-        printf("invalid salary: %.2f",gs);
-    }
+    // if(i>=100000){
+    //     da=i*80/100;
+    //     hra=i*20/100;
+    //     gs=i+da+hra;
+    //     printf("da: %.2f\n",da);
+    //     printf("hra: %.2f\n",hra);
+    //     printf("gross salary: %.2f\n",gs);
+    // }
+    // else if(i>=20000 && i<100000){
+    //     da=i*90/100;
+    //     hra=i*25/100;
+    //     gs=i+da+hra;
+    //     printf("da: %.2f\n",da);
+    //     printf("hra: %.2f\n",hra);
+    //     printf("gross salary: %.2f\n",gs);
+    // }
+    // else if(i>=30000 && i<20000){
+    //     da=i*95/100;
+    //     hra=i*30/100;
+    //     gs=i+da+hra;
+    //     printf("da: %.2f\n",da);
+    //     printf("hra: %.2f\n",hra);
+    //     printf("gross salary: %.2f\n",gs);
+    // }
+    // else{
+    //     printf("invalid salary: %.2f",gs);
+    // }
 
-    // *****Question 21******
+    // // *****Question 21******
     float i, a=0.50,e,b;
     printf("Enter electricity unit: ");
     scanf("%f",&i);
