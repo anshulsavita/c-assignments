f7cb923611350c3b3b1894d024215d2632b9cd1a
Anshul savita <anshulsavita370@gmail.com>
2026-04-22 16:54:52 +0530

    Added some questions of arrays in arrays assignment

--- array assignment/Q16.c
+++ array assignment/Q16.c
@@ -102,49 +102,331 @@ void main(){
     
     // ****Question 21***
     // -->Write a c program to sort even and odd elements of array separately.
-    int x[6],y[6],z[6],i,o=0,e=0,j,t;
-    for(i=0;i<=5;i++){
-        printf("Enter value @ %d ",i);
-        scanf("%d",&x[i]);
-    }
-    e=0;
-    o=0;
-    for(i=0;i<=5;i++){
-        if(x[i]%2==0){
-            y[e]=x[i];
-            e++;
-        }
-        else{
-            z[o]=x[i];
-            o++;
-        }
-    }
-    for(i=0;i<=4;i++){
-        for(j=0;j<e-i;j++){
-            if(y[j]>y[j+1]){
-                t=y[j];
-                y[j]=y[j+1];
-                y[j+1]=t;
-            }
-        }
-    }
-    for(i=0;i<=4;i++){
-        for(j=0;j<o-i;j++){
-            if(z[j]>z[j+1]){
-                t=z[j];
-                z[j]=z[j+1];
-                z[j+1]=t;
-            }
-        }
-    }
-    printf("even elements:\n");
-    for(i=0;i<e;i++){
-        printf("%d,",y[i]);
-    }
+    // int x[6],y[6],z[6],i,o=0,e=0,j,t;
+    // for(i=0;i<=5;i++){
+    //     printf("Enter value @ %d ",i);
+    //     scanf("%d",&x[i]);
+    // }
+    // e=0;
+    // o=0;
+    // for(i=0;i<=5;i++){
+    //     if(x[i]%2==0){
+    //         y[e]=x[i];
+    //         e++;
+    //     }
+    //     else{
+    //         z[o]=x[i];
+    //         o++;
+    //     }
+    // }
+    // for(i=0;i<=4;i++){
+    //     for(j=0;j<e-i;j++){
+    //         if(y[j]>y[j+1]){
+    //             t=y[j];
+    //             y[j]=y[j+1];
+    //             y[j+1]=t;
+    //         }
+    //     }
+    // }
+    // for(i=0;i<=4;i++){
+    //     for(j=0;j<o-i;j++){
+    //         if(z[j]>z[j+1]){
+    //             t=z[j];
+    //             z[j]=z[j+1];
+    //             z[j+1]=t;
+    //         }
+    //     }
+    // }
+    // printf("even elements:\n");
+    // for(i=0;i<e;i++){
+    //     printf("%d,",y[i]);
+    // }
 
-    printf("\nOdd elements:\n");
-    for(i=0;i<o;i++){
-        printf("%d,",z[i]);
-    }
+    // printf("\nOdd elements:\n");
+    // for(i=0;i<o;i++){
+    //     printf("%d,",z[i]);
+    // }
 
+    // ****Question 22****
+    // --Write a c program to add two matrices.
+    // int x[3][3],y[3][3],z[3][3];
+    // int i,j;
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter value of string 1 @ %d,%d:",i,j);
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter value of string 2 @ %d,%d:",i,j);
+    //         scanf("%d",&y[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         z[i][j]=x[i][j]+y[i][j];
+    //         printf("%d ",z[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // ****Question 23****
+    // --Write a c program to subtract two matrices.
+    // int x[3][3],y[3][3],z[3][3];
+    // int i,j;
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter value of string 1 @ %d,%d:",i,j);
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter value of string 2 @ %d,%d:",i,j);
+    //         scanf("%d",&y[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         z[i][j]=x[i][j]-y[i][j];
+    //         printf("%d ",z[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // ****Question 24****
+    // --Write a c program to perform scalar matrix multiplication.
+    // int x[3][3],y[3][3],z[3][3],i,j,k,s;
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter value of string 1 @ %d,%d:",i,j);
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter value of string 2 @ %d,%d:",i,j);
+    //         scanf("%d",&y[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         z[i][j]=x[i][j]*y[i][j];
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("%d ",z[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // ****Question 25***
+    // --Write a c program to multiply two matrix.
+    // int x[3][3],y[3][3],z[3][3],i,j;
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter value of string 1 @ %d,%d:",i,j);
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter value of string 2 @ %d,%d:",i,j);
+    //         scanf("%d",&y[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         s=0;
+    //         for(k=0;k<=2;k++){
+    //             s=s+(x[i][k]*y[k][j]);
+    //         }
+    //         z[i][j]=s;
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf(" %d",z[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // ****Question 26****
+    // --Write a cprogram to check whether two matrix are equal or not.
+    // int x[3][3],y[3][3],z[3][3],i,j,c=0,t=0;
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter value of string 1 @ %d,%d:",i,j);
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter value of string 2 @ %d,%d:",i,j);
+    //         scanf("%d",&y[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         t++;
+    //         if(x[i][j]==y[i][j]){
+    //             c++;
+    //         }
+    //     }
+    // }
+    // if(c==t){
+    //     printf("Both are equal..");
+    // }
+    // else{
+    //     printf("not equal..");
+    // }
+    
+    // ****Question 27****
+    // ---Write a c program to find sum of main diagonal element of a matrix.
+    // int x[3][3];
+    // int i,j,s=0;
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter values @ %d,%d:",i,j);
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         if(i==j){
+    //             s=s+x[i][j];
+    //         }
+    //     }
+    // }
+    // printf("%d",s);
+
+    // ****Question 28****
+    // ---Write a c program to find sum of minor diagonal elements of a matrix.
+    // int x[3][3];
+    // int i,j,s=0;
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter values");
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     s=s+x[i][2-i];
+    // }
+    // printf("%d",s);
+
+    // ****Question 29****
+    // ---Write a c program to find sum of each row and column of a matrix.
+    // int x[3][3];
+    // int i,j,r=0,c=0;
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter value @ %d,%d:",i,j);
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     s=0;
+    //     for(j=0;j<=2;j++){
+    //         printf("%d ",x[i][j]);
+    //         r=r+x[i][j];
+    //     }
+    //     printf("%d \n",r);
+    // }
+    // for(i=0;i<=2;i++){
+    //     s=0;
+    //     for(j=0;j<=2;j++){
+    //         printf("%d ",x[i][j]);
+    //         c=c+x[j][i];
+    //     }
+    //     printf("%d",c);
+    //     printf("\n");
+    // }
+
+    // ****Question 30***
+    // ---Write a c program to interchange diagonals of a matrix.
+    // int x[3][3];
+    // int i,j,t;
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter value @ %d,%d:",i,j);
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         if(i==0 || i==2){
+    //             if(j==0){
+    //                 t=x[i][j];
+    //                 x[i][j]=x[i][j+2];
+    //                 x[i][j+2]=t;
+    //             }
+    //         }
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("%d ",x[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // ****Question 31****
+    // ---Write a c program to find upper triangular matrix.
+    // int i,j,x[3][3];
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter values @ %d,%d: ",i,j);
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=i;j++){
+    //         printf("  ");
+    //     }
+    //     for(j=i;j<=2;j++){
+    //         printf("%d ",x[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // ****Question 32***
+    // --Write a c program to find lower triangular matrix.
+    // int i,j,x[3][3];
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter values @ %d,%d: ",i,j);
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=i;j++){
+    //         printf("%d ",x[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // ****Question 33****
+    // --Write a c program to sum of upper triangle matrix.
+    // int i,j,x[3][3],s=0;
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter values @ %d,%d: ",i,j);
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=i;j++){
+    //         printf("  ");
+    //     }
+    //     for(j=i;j<=2;j++){
+    //         s=s+x[i][j];
+    //     }
+    // }
+    // printf("sum: %d",s);
+
+    // ****Question 34****
+    // ---Write a c program to find transpose of a matrix.
 }
